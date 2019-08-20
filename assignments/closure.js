44a9e904ca86f969061ccab90dc80a5a5c5c598e
Robert <rc3331@gmail.com>
2019-08-20 13:48:19 -0500

    committing mvp and stretch

--- assignments/closure.js
+++ assignments/closure.js
@@ -3,13 +3,32 @@
 // Keep it simple! Remember a closure is just a function
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
+function closure() {
+  let x = 10
 
+  function child() {
+    let y = 15
+
+    function grandchild() {
+      let z = 1
+      console.log(x + y + z)
+    }
+    grandchild()
+  }
+  child()
+}
+closure()
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Implement a "counter maker" function ====
 const counterMaker = () => {
+  let count = 0;
+  function counter(){
+    return count++
+  }
+  return counter()
   // IMPLEMENTATION OF counterMaker:
   // 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
   // 2- Declare a function `counter`. It should increment and return `count`.
@@ -24,9 +43,24 @@ const counterMaker = () => {
 // ==== Challenge 3: Make `counterMaker` more sophisticated ====
 // It should have a `limit` parameter. Any counters we make with `counterMaker`
 // will refuse to go over the limit, and start back at 1.
+const counterMaker = (limit) => {
+  let count = 0;
+  function counter(){
+    return count++
+  }
+  if (count >= limit){
+    count = 1
+    return counterMaker(limit)
+  }
+  return counter()
+}
 
 // ==== Challenge 4: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
+  const obj = {
+    inc: count => count ++,
+    dec: count => count --,
+  }
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
